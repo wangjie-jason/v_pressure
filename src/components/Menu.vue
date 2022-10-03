afcd354453170a8973d2a7824b2996c5a37616f6
汪杰 <wangjie@wangjiedeMacBook-Pro.local>
2022-10-03 16:38:21 +0800

    前端项目新增项目管理功能

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -12,7 +12,7 @@
           <i class="el-icon-menu"></i>
           <span>功能区</span>
         </template>
-        <el-menu-item index="/project_management">项目管理</el-menu-item>
+        <el-menu-item @click="project_list_visible=true">项目管理</el-menu-item>
         <el-menu-item index="/task_panel">任务面板</el-menu-item>
         <el-menu-item index="/env_set">环境设置</el-menu-item>
       </el-submenu>
@@ -30,14 +30,36 @@
       </el-submenu>
       <el-menu-item @click="logout" style="color: grey;font-size: xx-small"> 退出 {{ username }}</el-menu-item>
     </el-menu>
+    <el-dialog title="项目列表" :visible.sync="project_list_visible">
+      <el-table :data="projects">
+        <el-table-column property="id" label="ID" width="50px"></el-table-column>
+        <el-table-column property="name" label="项目名称"></el-table-column>
+        <el-table-column width="150px">
+          <template slot="header">
+            <el-button style="width: 121px" @click="add_project">新增项目</el-button>
+          </template>
+          <template slot-scope="scope">
+            <router-link :to="'project_detail/?project_id='+scope.row.id">
+              <el-button size="mini" type="success">进入</el-button>
+            </router-link>
+            &nbsp
+            <el-button size="mini" type="danger" @click="delete_project(scope.row.id)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Menu",
   data() {
     return {
+      projects: [],
+      project_list_visible: false,
       username: '',
       now_path: window.location.href.split('#')[1].split('?')[0]
     }
@@ -47,11 +69,28 @@ export default {
     if (this.username == null) {
       window.location.href = '/'
     }
+    axios.get('/get_projects/').then(res => {
+      this.projects = res.data
+    })
   },
   methods: {
     logout() {
       sessionStorage.clear()
       window.location.href = '/'
+    },
+    add_project() {
+      axios.get('/add_project/').then(res => {
+        this.projects = res.data
+      })
+    },
+    delete_project(project_id) {
+      axios.get('/delete_project/', {
+        params: {
+          project_id: project_id
+        }
+      }).then(res => {
+        this.projects = res.data
+      })
     }
   }
 }
